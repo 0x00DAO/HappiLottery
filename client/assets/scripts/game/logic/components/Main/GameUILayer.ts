bcfd5234d725f21cb0cfb8eb138c6e05f9f01ffb
enixlee <405284695@qq.com>
2023-09-07 16:04:30 +0800

    add your faction

--- client/assets/scripts/game/logic/components/Main/GameUILayer.ts
+++ client/assets/scripts/game/logic/components/Main/GameUILayer.ts
@@ -1,15 +1,28 @@
-import { Label, _decorator } from "cc";
+import { Label, _decorator, Node, Sprite } from "cc";
 import { GameObject } from "../../../core/game/GameObject";
 import { gameAccountData } from "../../data/GameAccountData";
 import { StringUtil } from "../../../core/utils/StringUtil";
 import { OnEvent } from "../../../core/event/decorators/OnEventDecorator";
 import { GameEventBuildArcadeAccount } from "../../events/GameEventBuildArcadeAccount";
+import { GameEventContractAirVoyageGameFinished } from "../../contracts/events/GameEventContractAirVoyageGameFinished";
+import { GameEventContractAirVoyageGameStarted } from "../../contracts/events/GameEventContractAirVoyageGameStarted";
+import { ViewUtil } from "../../../core/utils/ViewUtil";
+import { Textures } from "../../enum/Textures";
+import { GameEventGameOpened } from "../../events/GameEventGameOpened";
+import { gameData } from "../../data/GameData";
+
 const { menu, ccclass, property } = _decorator;
 @ccclass("GameUILayer")
 @menu("game/logic/components/Main/GameUILayer")
 export class GameUILayer extends GameObject {
   @property(Label)
   private arcadeAccountLabel: Label = null!;
+
+  @property(Node)
+  private yourFactionNode: Node = null!;
+
+  @property(Sprite)
+  private factionSp: Sprite = null!;
 
   static prefabName(): string {
     return "GameUILayer";
@@ -23,15 +36,43 @@ export class GameUILayer extends GameObject {
     }
   }
 
-  @OnEvent(GameEventBuildArcadeAccount.event)
-  private onArcadeAccountBuilded() {
-    this.setArcadeAccount();
-  }
-
   private setArcadeAccount() {
     this.arcadeAccountLabel.string = `arcade account: ${StringUtil.shortAddress(
       gameAccountData.address as string,
       6
     )}`;
+  }
+
+  @OnEvent(GameEventBuildArcadeAccount.event)
+  private onArcadeAccountBuilded() {
+    this.setArcadeAccount();
+  }
+
+  @OnEvent(GameEventContractAirVoyageGameFinished.event)
+  private onGameFinished() {
+    this.yourFactionNode.active = false;
+  }
+  @OnEvent(GameEventGameOpened.eventAsync)
+  private async onGameStarted() {
+    if (!gameData.currentGame || !gameData.currentGame.me) {
+      this.yourFactionNode.active = false;
+      return Promise.resolve();
+    }
+    if (!gameData.currentGame.hasStarted) {
+      this.yourFactionNode.active = false;
+      return Promise.resolve();
+    }
+
+    const account = await gameAccountData.getPlayerSimple();
+    if (!account) {
+      this.yourFactionNode.active = false;
+      return Promise.resolve();
+    }
+
+    const seatIndex = account.seatIndex;
+    const planeTex = `airplane_${seatIndex}`;
+
+    ViewUtil.setTexture(this.factionSp, Textures.UI_GAME_MAP, planeTex);
+    this.yourFactionNode.active = true;
   }
 }
--- client/assets/scripts/game/logic/const/Game.ts
+++ client/assets/scripts/game/logic/const/Game.ts
@@ -1,6 +1,6 @@
 export const GAME_ID = 666;
 export const VERSION = {
-  version: "1.1.0.2",
+  version: "1.1.0.3",
   buildVersion: "2023-08-30.1",
   cacheVersion: "2023-08-30",
   dataVersion: "2022-08-30.1",
