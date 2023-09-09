ad02a4a371d3f297b0eb209b1270097f25309495
enixlee <405284695@qq.com>
2023-09-09 10:48:36 +0800

    fix bug

--- client/assets/scripts/game/logic/const/Game.ts
+++ client/assets/scripts/game/logic/const/Game.ts
@@ -1,6 +1,6 @@
 export const GAME_ID = 666;
 export const VERSION = {
-  version: "1.1.0.7",
+  version: "1.1.0.8",
   buildVersion: "2023-08-30.1",
   cacheVersion: "2023-08-30",
   dataVersion: "2022-08-30.1",
